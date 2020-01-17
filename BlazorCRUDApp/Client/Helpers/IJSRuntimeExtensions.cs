1af73ce67f839b58bd18e663a0721b1c5205da8b
Andy Stav <andy.stavrinou@rufusleonard.com>
2020-01-17 16:36:49 +0000

    Add project files.

---
+++ BlazorCRUDApp/Client/Helpers/IJSRuntimeExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace BlazorCRUDApp.Client.Helpers
+{
+    public static class IJSRuntimeExtensions
+    {
+        public static ValueTask SaveAs(this IJSRuntime js, string fileName, byte[] content)
+        {
+            return js.InvokeVoidAsync("saveAsFile", fileName, Convert.ToBase64String(content));
+        }
+
+        public static ValueTask DisplayMessage(this IJSRuntime js, string message)
+        {
+            return js.InvokeVoidAsync("Swal.fire",message);
+        }
+
+
+        public static ValueTask DisplayMessage(this IJSRuntime js, string title, string message, SweetAlertMessageType sweetAlertMessageType)
+        {
+            return js.InvokeVoidAsync("Swal.fire", title, sweetAlertMessageType.ToString());
+        }
+
+        public static ValueTask<bool> Confirm(this IJSRuntime js, string title, string message, SweetAlertMessageType sweetAlertMessageType)
+        {
+            return js.InvokeAsync<bool>("CustomConfirm", title,message, sweetAlertMessageType.ToString());
+        }
+    }
+
+    public enum SweetAlertMessageType
+    {
+        question, warning, error, success, info 
+    }
+}
